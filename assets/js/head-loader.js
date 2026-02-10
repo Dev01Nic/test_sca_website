9b94aeaec78a4b29e30be5fce44584302557db26
hiradon <hiradon.buhroy@nic.in>
2026-02-10 18:01:54 +0530

    added basepath loader

--- assets/js/head-loader.js
+++ assets/js/head-loader.js
@@ -1,3 +1,3 @@
 const base = document.createElement('base');
-base.href = window.location.hostname.includes('github.io') ? '/test_sca_website/' : '/';
+base.href = window.location.hostname.includes('github.io') ? '/test_sca_website/' : '/ttt';
 document.head.prepend(base);
